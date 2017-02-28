6c432f6241ac4f26426edb0496ad3fcacf27fcf2
Taulant Shatri <ts36599@ubt-uni.net>
2017-02-28 11:18:59 +0100

    taula

--- js/custom.js
+++ js/custom.js
@@ -28,18 +28,3 @@
         $( '.swipebox' ).swipebox();
 
     } )( jQuery );
-
-    /*back to top*/
-    $(document).ready(function(){
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 100) {
-                $('#scroll').fadeIn();
-            } else {
-                $('#scroll').fadeOut();
-            }
-        });
-        $('#scroll').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 600);
-            return false;
-        });
-    });
